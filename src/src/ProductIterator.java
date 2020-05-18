bf48e917b3903b7401546b50d9c440c60ac8d097
domi-dominika <dominika.jarek18@gmail.comgit config --global user.name Dominika4git config --global user.email dominika.jarek18@gmail.com>
2020-05-18 12:51:55 +0200

    empty classes

---
+++ src/src/ProductIterator.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+public class ProductIterator implements Iterator<Product> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Product next() {
+        return null;
+    }
+}
