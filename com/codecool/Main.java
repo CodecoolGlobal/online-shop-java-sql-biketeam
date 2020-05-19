e4e3ee498cab02d23207c5470b6e3eb99b30c893
mikolaj <mikolaj.urbanek@gmail.com>
2020-05-19 13:52:24 +0200

    Program finally is starting to work!

--- com/codecool/Main.java
+++ com/codecool/Main.java
@@ -2,17 +2,22 @@ package com.codecool;
 
 //import com.jakewharton.fliptables.FlipTableConverters;
 
-    public class Main {
-        public static void main(String[] args) {
-//            CustomerDao customerDao = new CustomerDao();
-//            List<Customer> customers = customerDao.getCustomers();
-//
-//            for (Customer customer : customers) {
-//                System.out.println(customer.getLogin() + " " + customer.getPassword());
-//            }
+import com.codecool.dao.CustomerDao;
+import com.codecool.models.Customer;
 
-            // pretty table
-   //         System.out.println(FlipTableConverters.fromIterable(users, User.class));
+import java.util.List;
+
+public class Main {
+        public static void main(String[] args) {
+            CustomerDao customerDao = new CustomerDao();
+            List<Customer> customers = customerDao.getCustomers();
+
+            for (Customer customer : customers) {
+                System.out.println(customer.getLogin() + " " + customer.getPassword());
+            }
+
+       //      pretty table
+    //        System.out.println(FlipTableConverters.fromIterable(users, User.class));
 
         }
     }
