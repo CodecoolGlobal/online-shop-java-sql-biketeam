1f632cd0e487e947c2ec9b24c3c698f280bc28c5
Neriso <Nerisox@gmail.com>
2020-05-20 11:32:41 +0200

    AdminDao created

---
+++ src/main/java/com/codecool/dao/AdminDao.java
@@ -0,0 +1,34 @@
+package com.codecool.dao;
+
+import com.codecool.models.Admin;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminDao extends Dao {
+
+    public List<Admin> getCustomers() {
+        List<Admin> customers = new ArrayList<>();
+        connect();
+
+        try {
+            ResultSet results = statement.executeQuery("SELECT * FROM Customer;");
+            while (results.next()) {
+                int id = results.getInt("Customer_ID");
+                String login = results.getString("Login");
+                String password = results.getString("Password");
+
+                Admin user = new Admin(id, login, password);
+                customers.add(user);
+            }
+            results.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return customers;
+    }
+}
