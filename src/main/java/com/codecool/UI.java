982b696be0b0506ce506a937eabdca79b6f0e692
Dominika <58705418+dominikajarek@users.noreply.github.com>
2020-05-21 13:54:45 +0200

    Update UI.java
    flip table of bikes from db

--- src/main/java/com/codecool/UI.java
+++ src/main/java/com/codecool/UI.java
@@ -1,12 +1,13 @@
 package com.codecool;
 
-import com.codecool.dao.BikeDao;
-import com.codecool.models.Bike;
+import com.codecool.dao.Dao;
 import com.jakewharton.fliptables.FlipTable;
+import com.jakewharton.fliptables.FlipTableConverters;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class UI {
+public class UI extends Dao {
 
     public static void login() {
         String[] headers = {"Welcome to our store!"};
@@ -20,8 +21,8 @@ public class UI {
     public static void menuForAdmin(){
         String[] headers = {"Hello Master what you want to do ?"};
         String[][] data = {
-            {"[1] Create new product category"},
-            {"[2] Edit name of the product"},
+                {"[1] Create new product category"},
+                {"[2] Edit name of the product"},
                 {"[3] Deactivate product"},
                 {"[4] Delete product"},
                 {"[5] Create new product"},
@@ -49,22 +50,9 @@ public class UI {
         System.out.println(FlipTable.of(headers,data));
     }
 
-    public static void displayBikes() {
-        String[] headers = {"Our Bikes to sell: "};
-        BikeDao bikeDao = new BikeDao();
-        List<Bike> bikes = bikeDao.getBikes();
-
-        for (Bike bike : bikes) {
-            String[][] data  = {
-                    {String.format("%s", bike.getId())},
-                    {String.format("%s", bike.getBrand())}
-            };
-            System.out.println(FlipTable.of(headers,data));
-            //System.out.println(bike.getId() + " " + bike.getBrand() + " | " + bike.getType() + " | " + bike.getColor() + " | In stock: " + bike.getInStock() + " | Price: " + bike.getPrice());
-
-        }
-
+    public void displayBikes() throws SQLException {
+        connect();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM Bike;");
+        System.out.println(FlipTableConverters.fromResultSet(resultSet));
     }
-
-
 }
